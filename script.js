d680052ceffed8322a39bb91ca6d35178cffc3ee
Reihan Mursyidi <mursyidireihan@gmail.com>
2026-01-06 21:17:49 +0700

    Refactor authentication and UI update functions

--- script.js
+++ script.js
@@ -424,7 +424,7 @@ function generateRapor() {
 
     document.getElementById('result-title').innerHTML = `<i class="fa-solid fa-pen-nib text-blue"></i> Catatan Rapor (${nama})`;
 
-    sendRequestAuth('generate-rapor', data);
+    sendRequest('generate-rapor', data);
 }
 
 // FITUR COPY TEXT
@@ -442,4 +442,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // 2. Cek apakah user sudah login sebelumnya?
     checkSession(); 
+
 });
